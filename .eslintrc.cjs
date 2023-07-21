38d5142c02ed68ab1ccbab938e92b37f6cc199b9
AlexRyshkov <alexryshkov72@gmail.com>
2023-07-21 04:10:39 +0300

    feat: setup eslint + prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,20 +8,23 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'eslint-config-prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: ['./tsconfig.json', './tsconfig.node.json'],
     tsconfigRootDir: __dirname,
-   },
-}
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
